f5042f3a75f8ac52ca9bcf3e10373857eabc12c4
Jesús Ariza <0619966879@uma.es>
2023-02-15 20:06:44 +0100

    added a test with multiple people and added the comments for each test case

--- src/test/java/org/uma/mps/jesus/PersonTest.java
+++ src/test/java/org/uma/mps/jesus/PersonTest.java
@@ -9,9 +9,23 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+@author Jesús Ariza Pomares
+Test cases:
+1. Checks if the name that's been given to a person is saved/stored properly.
+2. Checks if the age that's been given to a person is saved/stored properly.
+3. Checks if the gender (man) that's been given to a person is saved/stored properly.
+4. Checks if the gender (woman) that's been given to a person is saved/stored properly.
+5. Checks if when only one man is added to a list, the mean gets calculated properly.
+6. Checks if when only one woman is added to a list, the mean gets calculated properly.
+7. Checks if when one man and one woman are added to a list, both means get calculated properly.
+8. Checks if a list with multiple men and multiple women returns the means for both genders properly.
+9. Checks if a negative value for the age of man is treated correctly.
+10. Checks if a negative value for the age of a woman is treated correctly.
+ */
+
 class PersonTest {
-    Person person1;
-    Person person2;
+    Person person1, person2, person3, person4, person5, person6;
     Person personNegativeAge;
     List<Person> listOfPeople;
     @BeforeEach
@@ -19,6 +33,10 @@ class PersonTest {
     {
         person1 = new Person("Jesús", 22, "Man");
         person2 = new Person("María Jesús", 22, "Woman");
+        person3 = new Person("JesúsDos", 44, "Man");
+        person4 = new Person("María JesúsDos", 44, "Woman");
+        person5 = new Person("JesúsDos María", 53, "Woman");
+        person6 = new Person("Ultima", 19, "Woman");
         personNegativeAge = new Person("Jesús Negativo", -22, "Man");
         listOfPeople = new ArrayList<Person>();
     }
@@ -93,6 +111,22 @@ class PersonTest {
         listOfPeople.add(person2);
         double[] obtainedValue = person2.averageAgePerGender(listOfPeople);
         double[] expectedValue = {22, 22};
+
+        assertArrayEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    void correctMeanWithMultipleMenAndMultipleWomen()
+    {
+        listOfPeople.add(person1);
+        listOfPeople.add(person2);
+        listOfPeople.add(person3);
+        listOfPeople.add(person4);
+        listOfPeople.add(person5);
+        listOfPeople.add(person6);
+
+        double[] obtainedValue = person1.averageAgePerGender(listOfPeople);
+        double[] expectedValue = {33, 34.5};
 
         assertArrayEquals(expectedValue, obtainedValue);
     }
